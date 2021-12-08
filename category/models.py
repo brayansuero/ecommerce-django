278e4b58f34297586ba2901f60b5df7e04846f37
brayanantoniosuero@gmail.com <brayanantoniosuero@gmail.com>
2021-12-08 10:26:43 -0500

    Este es mi primer paquete commit de mi proyecto

---
+++ category/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.urls import reverse
+
+# Create your models here.
+class Category(models.Model):
+    category_name = models.CharField(max_length=20, unique=True)
+    description = models.CharField(max_length=255, blank = True)
+    slug = models.CharField(max_length=20, unique=True)
+    cat_image = models.ImageField(upload_to = 'photos/categories', blank = True)
+
+
+
+    def get_url(self):
+        return reverse('products_by_category', args=[self.slug])
+
+    def __str__(self):
+        return self.category_name
